eb1706cdc68ecfb6cea86dd5253b0d34e81c0bf9
yuta1192 <yuta.s1182@gmail.com>
2021-11-13 10:12:40 +0900

    railsのJSONをnextjsで取得し表示する

--- portfolio-backend/db/seeds.rb
+++ portfolio-backend/db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Test.create!(
+  [
+    {
+      title: 'Next.js + Ruby on Rails + Docker の環境構築'
+    },
+    {
+      title: 'React Hooks でカスタムフックを作る'
+    },
+    {
+      title: 'GraphQL と Apollo Client 入門'
+    },
+    {
+      title: '【TypeScript4.3】Template Literal Types'
+    },
+    {
+      title: 'Tailwind CSS でダークモード実装'
+    },
+  ]
+)
