98be547d66a41ce70311dfc08fb31c9383520185
yuta1192 <yuta.s1182@gmail.com>
2021-11-20 23:32:26 +0900

    headerの途中

--- portfolio-frontend/app/pages/qiitas.tsx
+++ portfolio-frontend/app/pages/qiitas.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Qiita() {
   return (
     <>
+      <Header />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+      <Footer />
     </>
   );
 }
---
+++ portfolio-frontend/app/tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      blue: colors.blue,
+    },
+  },
+};
