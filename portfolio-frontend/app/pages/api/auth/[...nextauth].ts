65a6db92b7c648f34d81d3e97900ef4183be239a
yuta1192 <yuta.s1182@gmail.com>
2021-12-10 14:13:30 +0900

    ログイン画面の途中、あとはリダイレクト

--- portfolio-frontend/app/pages/api/auth/[...nextauth].ts
+++ portfolio-frontend/app/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 
-const findUserByCredentials = async (credentials) => {
+const findUserByCredentials = async (credentials: any) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/user_select`,
     {
